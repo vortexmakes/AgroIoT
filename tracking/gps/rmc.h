12197b5a9cbf9fedc8871055df7ad72a16fc8db5
Dario Baliña <dariosb@gmail.com>
2018-09-07 16:35:52 -0300

    working on GPS parser

--- tracking/gps/rmc.h
+++ tracking/gps/rmc.h
@@ -30,13 +30,17 @@ extern "C" {
 
 /* --------------------------------- Macros -------------------------------- */
 /* -------------------------------- Constants ------------------------------ */
-#define RMC_TIME_LEN        6
-#define RMC_LATITUDE_LEN    9
-#define RMC_LONGITUDE_LEN   9
-#define RMC_SPEED_LEN       5
-#define RMC_TRACK_LEN       5
-#define RMC_DATE_LEN        6
-#define RMC_MAGVAR_LEN      5
+#define RMC_TIME_INT_LEN        6
+#define RMC_TIME_DEC_LEN        2
+#define RMC_LATITUDE_INT_LEN    4
+#define RMC_LATITUDE_DEC_LEN    5
+#define RMC_LONGITUDE_INT_LEN   5
+#define RMC_LONGITUDE_DEC_LEN   5
+#define RMC_SPEED_INT_LEN       3
+#define RMC_SPEED_DEC_LEN       3
+#define RMC_TRACK_LEN           5
+#define RMC_DATE_LEN            6
+#define RMC_MAGVAR_LEN          5
 
 #define RMC_StatusVoid      'V'
 #define RMC_StatusActive    'A'
@@ -44,12 +48,16 @@ extern "C" {
 /* ------------------------------- Data types ------------------------------ */
 typedef struct
 {
-    char time[RMC_TIME_LEN+1];
+    char timeInt[RMC_TIME_INT_LEN+1];
+    char timeDec[RMC_TIME_DEC_LEN+1];
     char status;
-    char latitude[RMC_LATITUDE_LEN+1];
+    char latitudeInt[RMC_LATITUDE_INT_LEN+1];
+    char latitudeDec[RMC_LATITUDE_DEC_LEN+1];
     char latitudeInd;
-    char longitude[RMC_LONGITUDE_LEN+1];
-    char speed[RMC_SPEED_LEN+1];
+    char longitudeInt[RMC_LONGITUDE_INT_LEN+1];
+    char longitudeDec[RMC_LONGITUDE_DEC_LEN+1];
+    char speedInt[RMC_SPEED_INT_LEN+1];
+    char speedDec[RMC_SPEED_DEC_LEN+1];
     char track[RMC_TRACK_LEN+1];
     char date[RMC_DATE_LEN+1];
     char MagneticOff[RMC_MAGVAR_LEN+1];
--- tracking/parser/ubxm8/ubxm8parser.c
+++ tracking/parser/ubxm8/ubxm8parser.c
@@ -30,19 +30,126 @@
 static rui8_t ubxm8parser;
 static RMC_t rmcCurrent, rmcLast;
 static RmcEvt rmcEvt;
+static char *p;
+
+SSP_DCLR_NORMAL_NODE rootGpsParser;
+SSP_DCLR_TRN_NODE rmcTimeInt, rmcTimeDec, rmcStatus, 
+                  rmcLongitudeInt, rmcLongitudeDec, rmcLongitudInd, 
+                  rmcLatitudeInt, rmcLatitudeDec, rmcLatitudeInd,
+                  rmcSpeedInt, rmcSpeedDec, rmcTrack, rmcDate,
+                  rmcMagneticOff, rmcMagneticInd, rmcCheckSum;
 
 /* ----------------------- Local function prototypes ----------------------- */
 static void rmcUpdate(unsigned char pos);
+static void toRmcTimeInt(unsigned char pos);
+static void rmcTimeIntCollect(unsigned char c);
+static void toRmcTimeDec(unsigned char pos);
+static void rmcTimeDecCollect(unsigned char c);
+static void toRmcStatus(unsigned char pos);
+static void rmcStatusCollect(unsigned char c);
+static void toRmcLongitudeInt(unsigned char pos);
+static void rmcLongitudeIntCollect(unsigned char c);
+static void toRmcLongitudeDec(unsigned char pos);
+static void toRmcLongitudeInd(unsigned char pos);
+static void toRmcLatitudeInt(unsigned char pos);
+static void toRmcLatitudeDec(unsigned char pos);
+static void toRmcLatitudeInd(unsigned char pos);
+static void toSpeed(unsigned char pos);
+static void toTrack(unsigned char pos);
+static void toDate(unsigned char pos);
+static void toMagneticOff(unsigned char pos);
+static void toMagneticOffInd(unsigned char pos);
 
 /* ---------------------------- Local functions ---------------------------- */
 
 SSP_CREATE_NORMAL_NODE(rootGpsParser);
 SSP_CREATE_BR_TABLE(rootGpsParser)
-	SSPBR("$GNRMC,",      rmcUpdate,     &rootGpsParser),
+	//SSPBR("$GNRMC,",      rmcUpdate,      &rootGpsParser),
+	SSPBR("$GNRMC,",    toRmcTimeInt,  &rmcTimeInt),
+SSP_END_BR_TABLE
+
+SSP_CREATE_TRN_NODE(rmcTimeInt, rmcTimeIntCollect);
+SSP_CREATE_BR_TABLE(rmcTimeInt)
+	SSPBR(".",      toRmcTimeDec,  &rmcTimeDec),
+	SSPBR("\r\n",   toRmcTimeDec,  &rootGpsParser),
+SSP_END_BR_TABLE
+
+SSP_CREATE_TRN_NODE(rmcTimeDec, rmcTimeDecCollect);
+SSP_CREATE_BR_TABLE(rmcTimeDec)
+	SSPBR(",",      toRmcStatus,  &rmcStatus),
+	SSPBR("\r\n",   toRmcTimeDec,  &rootGpsParser),
+SSP_END_BR_TABLE
+
+SSP_CREATE_TRN_NODE(rmcStatus, rmcStatusCollect);
+SSP_CREATE_BR_TABLE(rmcStatus)
+	SSPBR(",",   toRmcLongitudeInt,  &rmcLongitudeInt),
+	SSPBR("\r\n",   toRmcTimeDec,  &rootGpsParser),
+SSP_END_BR_TABLE
+
+SSP_CREATE_TRN_NODE(rmcLongitudeInt, rmcLongitudeIntCollect);
+SSP_CREATE_BR_TABLE(rmcLongitudeInt)
 SSP_END_BR_TABLE
 
 static void
-rmcUpdate(unsigned char pos)
+toRmcTimeInt(unsigned char pos)
+{
+    (void)pos;
+
+    memset(&rmcCurrent, '\0', sizeof(rmcCurrent));
+    p = rmcCurrent.timeInt;
+}
+
+static void
+rmcTimeIntCollect(unsigned char c)
+{
+    (void)c;
+}
+
+static void
+toRmcTimeDec(unsigned char pos)
+{
+    (void)pos;
+
+}
+
+static void
+rmcTimeDecCollect(unsigned char c)
+{
+    (void)c;
+}
+
+
+static void
+toRmcStatus(unsigned char pos)
+{
+    (void)pos;
+
+    p = &rmcCurrent.status;
+}
+
+static void
+rmcStatusCollect(unsigned char pos)
+{
+    (void)pos;
+
+    p = &rmcCurrent.status;
+}
+
+static void
+toRmcLongitudeInt(unsigned char pos)
+{
+    (void)pos;
+
+}
+
+static void
+rmcLongitudeIntCollect(unsigned char pos)
+{
+    (void)pos;
+
+}
+static void
+rmcLast_update(unsigned char pos)
 {
     (void)pos;
 
