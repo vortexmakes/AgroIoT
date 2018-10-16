ad32f568f94d5a0af9373df4110ab8838fe1c5bb
lf <francuccilea@gmail.com>
2018-10-16 15:15:52 -0300

    Working on job conditions

---
+++ tracking/device/inc/jobcond.h
@@ -0,0 +1,48 @@
+/**
+ *  \file       jobcond.h
+ *  \brief      Specifies the interface of JobCond module
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2018.16.10  LeFr  v1.0.00  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  lf@vortexmakes.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* --------------------------------- Module -------------------------------- */
+#ifndef __JOBCOND_H__
+#define __JOBCOND_H__
+
+/* ----------------------------- Include files ----------------------------- */
+/* ---------------------- External C language linkage ---------------------- */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* --------------------------------- Macros -------------------------------- */
+/* -------------------------------- Constants ------------------------------ */
+/* ------------------------------- Data types ------------------------------ */
+typedef struct JobCond JobCond;
+typedef int (*TestOper)(JobCond *const me);
+
+struct JobCond
+{
+    TestOper test;
+};
+
+/* -------------------------- External variables --------------------------- */
+/* -------------------------- Function prototypes -------------------------- */
+/* -------------------- External C language linkage end -------------------- */
+#ifdef __cplusplus
+}
+#endif
+
+/* ------------------------------ Module end ------------------------------- */
+#endif
+
+/* ------------------------------ End of file ------------------------------ */
--- tracking/device/test/test_device.c
+++ tracking/device/test/test_device.c
@@ -17,16 +17,36 @@
 /* ----------------------------- Include files ----------------------------- */
 #include "unity.h"
 #include "device.h"
+#include "jobcond.h"
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
+enum
+{
+    DEVA, DEVB, NUM_DEVS
+};
+
 /* ---------------------------- Local data types --------------------------- */
+typedef struct DevAJobCond DevAJobCond;
+struct DevAJobCond
+{
+    JobCond base;
+    int xMax;
+    int xMin;
+    int yMin;
+};
+
 /* ---------------------------- Global variables --------------------------- */
 /* ---------------------------- Local variables ---------------------------- */
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
+static int 
+DevA_test(JobCond *const me)
+{
+    return 1;
+}
+
 /* ---------------------------- Global functions --------------------------- */
-/* =========================== Bittbl test group =========================== */
 void 
 setUp(void)
 {
@@ -38,9 +58,12 @@ tearDown(void)
 }
 
 void
-test_first(void)
+test_CtorInitAttr(void)
 {
-    TEST_IGNORE();
+    DevAJobCond devAJobCond;
+
+    device_ctor(DEVA, &(JobCond *)&devAJobCond);
+    TEST_ASSERT_
 }
 
 /* ------------------------------ End of file ------------------------------ */
