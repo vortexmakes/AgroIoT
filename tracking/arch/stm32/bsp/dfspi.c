2f6e6b3f81c419dd69cf4c539ae21997e35a157e
dariosb <dariosb@gmail.com>
2019-06-27 14:46:55 -0300

    Dataflash SPI on STM32 polling mode get DevID ok

--- tracking/arch/stm32/bsp/dfspi.c
+++ tracking/arch/stm32/bsp/dfspi.c
@@ -1,45 +1,75 @@
-/*
- *	dfspi.c
- *
- *	Dataflash SPI peripherical management.
- *
- *	Programmer: Darío S. Baliña.
- *	Date : 26/05/06.
+/**
+ *  \file       dfspi.c
+ *  \brief      Dataflash SPI peripheral for STM32.
  */
 
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2019.06.27  DaBa  v1.0.00  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  DaBa  Dario Baliña       db@vortexmakes.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
 #include "mytypes.h"
 #include "dfspi.h"
 #include "spi.h"
 
+/* ----------------------------- Local macros ------------------------------ */
+#define dfspi_cs(b)      HAL_GPIO_WritePin(MEM_CS_GPIO_Port, MEM_CS_Pin, b)
+
+/* ------------------------------- Constants ------------------------------- */
 #define	DUMMY_DATA	0xFF
 
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+static uchar spi_xfer(uchar b);
+
+/* ---------------------------- Local functions ---------------------------- */
 static
 uchar
 spi_xfer(uchar b)
 {
     uchar r;
+    HAL_StatusTypeDef res;
 
-    if(HAL_SPI_TransmitReceive(&hspi1, &b, &r, 1, 100) != HAL_OK)
-        return r;
-    else
-        return 0;
+    res = HAL_SPI_TransmitReceive(&hspi1, &b, &r, 1, 100);
+
+    return (res == HAL_OK) ? r : 0;
 }
 
+/* ---------------------------- Global functions --------------------------- */
 void
 dfspi_init(void)
 {
+    dfspi_cs(1);
+
     MX_SPI1_Init();
+
+    /* 
+     * STM32 not sets SCK signal to high after init, although CPOL is set to 1,
+     * then a dummy not CS asserted transfer is needed to force SPI_CLK
+     * go to high before first assertion of CS.
+     */
+    spi_xfer(DUMMY_DATA);
 }
 
 void
 dfspi_select_channel(void)
 {
+    dfspi_cs(0);
 }
 
 void
 dfspi_deselect_channel(void)
 {
-
+    dfspi_cs(1);
 }
 
 void
@@ -54,3 +84,4 @@ dfspi_get_byte(void)
     return spi_xfer(DUMMY_DATA);
 }
 
+/* ------------------------------ End of file ------------------------------ */
--- tracking/ffile/portable/dataflash/dfspi.h
+++ tracking/ffile/portable/dataflash/dfspi.h
@@ -1,17 +1,41 @@
-/*
- * 	dfspi.h
- * 		Dataflash SPI Hardware Abstraction Layer
+/**
+ *  \file       dfspi.h
+ *  \brief      Dataflash SPI interface
  */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2019.06.27  DaBa  v1.0.00  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  DaBa  Dario Baliña db@vortexmakes.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* --------------------------------- Module -------------------------------- */
 
 #ifndef __DFSPI_H__
 #define __DFSPI_H__
 
+/* ----------------------------- Include files ----------------------------- */
 #include "mytypes.h"
 
+/* ---------------------- External C language linkage ---------------------- */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* --------------------------------- Macros -------------------------------- */
+/* -------------------------------- Constants ------------------------------ */
 #define DFLASH_BASE				1
 #define DF_BOUNCE_TIME 			(50/SLEEP_BASE)
 #define DF_RELEASE_TIME 		(1000/DFLASH_BASE)
 
+/* ------------------------------- Data types ------------------------------ */
+/* -------------------------- External variables --------------------------- */
+/* -------------------------- Function prototypes -------------------------- */
 void dfspi_init(void);
 
 void dfspi_select_channel(void);
@@ -20,4 +44,10 @@ void dfspi_deselect_channel(void);
 void dfspi_send_byte(uchar b);
 uchar dfspi_get_byte(void);
 
+/* -------------------- External C language linkage end -------------------- */
+#ifdef __cplusplus
+}
+#endif
+
+/* ------------------------------ Module end ------------------------------- */
 #endif
