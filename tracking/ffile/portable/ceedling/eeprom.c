ddfeea37eba3a84be2b3e586bdb76ca517c8e037
lf <francuccilea@gmail.com>
2019-07-25 23:17:41 -0300

    Tested ffdir module for eeprom

---
+++ tracking/ffile/portable/ceedling/eeprom.c
@@ -0,0 +1,55 @@
+/**
+ *  \file       eeprom.c
+ *  \brief
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2019.25.07  LeFr  v1.0.00  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci lf@vortexmakes.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+#include "eeprom.h"
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
+void
+eeprom_init(void)
+{
+}
+
+void
+eeprom_write(uint8_t* p, uint16_t addr, uint16_t qty)
+{
+}
+
+void
+eeprom_read(uint8_t* p, uint16_t addr, uint16_t qty)
+{
+}
+
+uint8_t
+eeprom_readStatusRegister(void)
+{
+    return 0;
+}
+
+void
+eeprom_writeStatusRegister(uint8_t regval)
+{
+}
+
+/* ------------------------------ End of file ------------------------------ */
+
