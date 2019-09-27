8f1a76e67bd819d0d6722f582461cd26958396ac
dariosb <dariosb@gmail.com>
2019-09-27 13:46:37 -0300

    test-ffile unity on STM32 harness

--- build/stm32-test-ffile/testSetup.c
+++ build/stm32-test-ffile/testSetup.c
@@ -16,6 +16,7 @@
 /* --------------------------------- Notes --------------------------------- */
 /* ----------------------------- Include files ----------------------------- */
 #include "cubemx.h"
+#include "stdio.h"
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
@@ -25,6 +26,22 @@
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
 /* ---------------------------- Global functions --------------------------- */
+/*
+ * Unity printout on UART5 at CubeMX configuration (115200 8N1)
+ */
+int
+__io_putchar(int ch)
+{
+	while(HAL_UART_GetState(&huart5) != HAL_UART_STATE_READY);
+
+	HAL_UART_Transmit_DMA(&huart5, (uint8_t *)(&ch), 1);
+
+	return ch;
+}
+
+/*
+ * Pre main() call Unity hook for hardware initialization.
+ */
 void
 suiteSetUp(void)
 {
@@ -33,11 +50,22 @@ suiteSetUp(void)
     SystemClock_Config();
     MX_GPIO_Init();
     MX_DMA_Init();
-    MX_USART6_UART_Init();
-    MX_USART1_UART_Init();
+    MX_UART5_Init();
     MX_CAN1_Init();
     MX_ADC1_Init();
     MX_SPI3_Init();
+}
+
+/*
+ * Post main() call Unity.
+ */
+int
+suiteTearDown(int num_failures)
+{
+	while(1)
+		;
+
+	return 0;
 }
 
 /* ------------------------------ End of file ------------------------------ */
