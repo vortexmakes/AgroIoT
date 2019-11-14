b7af1bce4d71c3d8afc1673fc30158196bf4f153
lf <francuccilea@gmail.com>
2019-11-14 20:34:09 -0300

    Preparing CommMgr for testing

---
+++ src/CommMgr/test/test_CommMgr.c
@@ -0,0 +1,56 @@
+/**
+ *  \file       test_Collector.c
+ *  \brief      Unit test for state machine module.
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  lf@vortexmakes.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+#include "unity.h"
+#include "Collector.h"
+#include "Mock_CollectorAct.h"
+#include "Mock_rkhsma.h"
+#include "Mock_rkhsm.h"
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+static Collector *me;
+const RKHSmaVtbl rkhSmaVtbl =  /* Instantiate it because rkhsma is mocked */
+{
+    rkh_sma_activate,
+    rkh_sma_dispatch,
+    rkh_sma_post_fifo,
+    rkh_sma_post_lifo
+};
+
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
+void
+setUp(void)
+{
+    me = RKH_DOWNCAST(Collector, collector);
+}
+
+void
+tearDown(void)
+{
+}
+
+void
+test_OnlyTestsCompilingAndLinkingStateMachine(void)
+{
+}
+
+/* ------------------------------ End of file ------------------------------ */
