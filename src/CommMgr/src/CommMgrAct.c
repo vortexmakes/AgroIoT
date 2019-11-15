bb508fd49efe3a3cd616522dfc546273092c1206
lf <francuccilea@gmail.com>
2019-11-14 23:37:44 -0300

    Prepared CommMgr action testing environment

--- src/CommMgr/src/CommMgrAct.c
+++ src/CommMgr/src/CommMgrAct.c
@@ -18,7 +18,6 @@
 #include "signals.h"
 #include "CommMgr.h"
 #include "CommMgrAct.h"
-#include "bsp.h"
 #include "CommMgrActRequired.h"
 
 /* ----------------------------- Local macros ------------------------------ */
@@ -33,19 +32,18 @@
 /* ---------------------------- Local functions ---------------------------- */
 /* ............................ Effect actions ............................. */
 void 
-CommMgr_ToidleExt0(CommMgr *const me, RKH_EVT_T *pe)
+CommMgr_ToIdleExt0(CommMgr *const me, RKH_EVT_T *pe)
 {
-		
 	RKH_TR_FWK_AO(me);
 	RKH_TR_FWK_QUEUE(&RKH_UPCAST(RKH_SMA_T, me)->equeue);
-	RKH_TR_FWK_STATE(me, &idle);
-	RKH_TR_FWK_STATE(me, &active);
-	RKH_TR_FWK_STATE(me, &WaitSync);
-	RKH_TR_FWK_STATE(me, &SendingStatus);
-	RKH_TR_FWK_STATE(me, &ReceivingAck);
-	RKH_TR_FWK_STATE(me, &SendingEndOfHist);
-	RKH_TR_FWK_STATE(me, &SendingHist);
-	RKH_TR_FWK_STATE(me, &EndCycle);
+	RKH_TR_FWK_STATE(me, &CommMgr_Idle);
+	RKH_TR_FWK_STATE(me, &CommMgr_Active);
+	RKH_TR_FWK_STATE(me, &CommMgr_WaitSync);
+	RKH_TR_FWK_STATE(me, &CommMgr_SendingStatus);
+	RKH_TR_FWK_STATE(me, &CommMgr_ReceivingAck);
+	RKH_TR_FWK_STATE(me, &CommMgr_SendingEndOfHist);
+	RKH_TR_FWK_STATE(me, &CommMgr_SendingHist);
+	RKH_TR_FWK_STATE(me, &CommMgr_EndCycle);
 	RKH_TR_FWK_SIG(evNetConnected);
 	RKH_TR_FWK_SIG(evNetDisconnected);
 	RKH_TR_FWK_SIG(evSent);
@@ -54,91 +52,69 @@ CommMgr_ToidleExt0(CommMgr *const me, RKH_EVT_T *pe)
 	RKH_TR_FWK_SIG(evSendFail);
 	RKH_TR_FWK_SIG(evGStatus);
 	RKH_TR_FWK_TIMER(&me->tmEvtObj0.tmr);
-	#if 0
-		RKH_TR_FWK_OBJ_NAME(CommMgr_ToidleExt0, "ToidleExt0");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_idleToactiveExt1, "idleToactiveExt1");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_ReceivingAckToC1Ext6, "ReceivingAckToC1Ext6");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_ReceivingAckToEndCycleExt7, "ReceivingAckToEndCycleExt7");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_SendingEndOfHistToEndCycleExt9, "SendingEndOfHistToEndCycleExt9");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_SendingHistToC0Ext12, "SendingHistToC0Ext12");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_SendingHistToEndCycleExt13, "SendingHistToEndCycleExt13");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_C1ToEndCycleExt14, "C1ToEndCycleExt14");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_C3ToSendingHistExt18, "C3ToSendingHistExt18");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_activeToactiveLoc0, "activeToactiveLoc0");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_enWaitSync, "enWaitSync");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_enSendingStatus, "enSendingStatus");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_enReceivingAck, "enReceivingAck");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_enSendingEndOfHist, "enSendingEndOfHist");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_enSendingHist, "enSendingHist");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_exWaitSync, "exWaitSync");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_isCondC0ToSendingHist11, "isCondC0ToSendingHist11");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_isCondC1ToC215, "isCondC1ToC215");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_isCondC2ToSendingStatus16, "isCondC2ToSendingStatus16");
-		RKH_TR_FWK_OBJ_NAME(CommMgr_isCondC3ToSendingHist18, "isCondC3ToSendingHist18");
-	#endif
 	
-	init();
+	/*init();*/
 }
 
 void 
 CommMgr_idleToactiveExt1(CommMgr *const me, RKH_EVT_T *pe)
 {
-	activateSync();
+	/*activateSync();*/
 }
 
 void 
 CommMgr_ReceivingAckToC1Ext6(CommMgr *const me, RKH_EVT_T *pe)
 {
-	parseRecv();
+	/*parseRecv();*/
 }
 
 void 
 CommMgr_ReceivingAckToEndCycleExt7(CommMgr *const me, RKH_EVT_T *pe)
 {
-	recvFail();
+	/*recvFail();*/
 }
 
 void 
 CommMgr_SendingEndOfHistToEndCycleExt9(CommMgr *const me, RKH_EVT_T *pe)
 {
-	sendMsgFail();
+	/*sendMsgFail();*/
 }
 
 void 
 CommMgr_SendingHistToC0Ext12(CommMgr *const me, RKH_EVT_T *pe)
 {
-	nextSend();
+	/*nextSend();*/
 }
 
 void 
 CommMgr_SendingHistToEndCycleExt13(CommMgr *const me, RKH_EVT_T *pe)
 {
-	sendMsgFail();
+	/*sendMsgFail();*/
 }
 
 void 
 CommMgr_C1ToEndCycleExt14(CommMgr *const me, RKH_EVT_T *pe)
 {
-	parseError();
+	/*parseError();*/
 }
 
 void 
 CommMgr_C3ToSendingHistExt18(CommMgr *const me, RKH_EVT_T *pe)
 {
-	initSendBlock();
+	/*initSendBlock();*/
 }
 
 void 
 CommMgr_activeToactiveLoc0(CommMgr *const me, RKH_EVT_T *pe)
 {
-	updateStatus();
+	/*updateStatus();*/
 }
 
 /* ............................. Entry actions ............................. */
 void 
 CommMgr_enWaitSync(CommMgr *const me)
 {
-	enWaitSync();
+	/*enWaitSync();*/
 	RKH_SET_STATIC_EVENT(&me->tmEvtObj0, evTout0);
 	RKH_TMR_INIT(&me->tmEvtObj0.tmr, RKH_UPCAST(RKH_EVT_T, &me->tmEvtObj0), NULL);
 	RKH_TMR_ONESHOT(&me->tmEvtObj0.tmr, RKH_UPCAST(RKH_SMA_T, me), WaitTime0);
@@ -147,32 +123,32 @@ CommMgr_enWaitSync(CommMgr *const me)
 void 
 CommMgr_enSendingStatus(CommMgr *const me)
 {
-	sendStatus();
+	/*sendStatus();*/
 }
 
 void 
 CommMgr_enReceivingAck(CommMgr *const me)
 {
-	receive();
+	/*receive();*/
 }
 
 void 
 CommMgr_enSendingEndOfHist(CommMgr *const me)
 {
-	sendEndOfHist();
+	/*sendEndOfHist();*/
 }
 
 void 
 CommMgr_enSendingHist(CommMgr *const me)
 {
-	sendOneMsg();
+	/*sendOneMsg();*/
 }
 
 /* ............................. Exit actions .............................. */
 void 
 CommMgr_exWaitSync(CommMgr *const me)
 {
-	exWaitSync();
+	/*exWaitSync();*/
 	rkh_tmr_stop(&me->tmEvtObj0.tmr);
 }
 
@@ -180,25 +156,25 @@ CommMgr_exWaitSync(CommMgr *const me)
 rbool_t 
 CommMgr_isCondC0ToSendingHist11(CommMgr *const me, RKH_EVT_T *pe)
 {
-	return (isEndOfBlock()) ? true : false;
+	/*return (isEndOfBlock()) ? true : false;*/
 }
 
 rbool_t 
 CommMgr_isCondC1ToC215(CommMgr *const me, RKH_EVT_T *pe)
 {
-	return (isAck()) ? true : false;
+	/*return (isAck()) ? true : false;*/
 }
 
 rbool_t 
 CommMgr_isCondC2ToSendingStatus16(CommMgr *const me, RKH_EVT_T *pe)
 {
-	return (isPending()) ? true : false;
+	/*return (isPending()) ? true : false;*/
 }
 
 rbool_t 
 CommMgr_isCondC3ToSendingHist18(CommMgr *const me, RKH_EVT_T *pe)
 {
-	return (isThereMsg()) ? true : false;
+	/*return (isThereMsg()) ? true : false;*/
 }
 
 /* ---------------------------- Global functions --------------------------- */
--- src/CommMgr/test/test_CommMgrAct.c
+++ src/CommMgr/test/test_CommMgrAct.c
@@ -10,13 +10,14 @@
 /* -------------------------------- Authors -------------------------------- */
 /*
  *  DaBa  Dario Baliña db@vortexmakes.com
+ *  LeFr  Leandro Francucci  lf@vortexmakes.com
  */
 
 /* --------------------------------- Notes --------------------------------- */
 /* ----------------------------- Include files ----------------------------- */
 #include "unity.h"
-#include "CommMgrAct.h"
 #include "CommMgr.h"
+#include "CommMgrAct.h"
 #include "signals.h"
 #include "rkhfwk_cast.h"
 #include "Mock_rkhtmr.h"
@@ -26,12 +27,26 @@
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
+RKHROM RKH_SBSC_T CommMgr_Idle, CommMgr_WaitSync, CommMgr_SendingStatus, 
+                  CommMgr_ReceivingAck, CommMgr_SendingEndOfHist, 
+                  CommMgr_SendingHist, CommMgr_EndCycle;
+RKHROM RKH_SCMP_T CommMgr_Active;
+RKHROM RKH_SCHOICE_T CommMgr_C0, CommMgr_C1, CommMgr_C2, CommMgr_C3;
+
 /* ---------------------------- Local data types --------------------------- */
 /* ---------------------------- Global variables --------------------------- */
 /* ---------------------------- Local variables ---------------------------- */
-RKH_SMA_CREATE(CommMgr, commMgr, 0, HCAL, NULL, NULL, NULL);
+RKH_SMA_CREATE(CommMgr, commMgr, 0, HCAL, NULL, CommMgr_ToIdleExt0, NULL);
 RKH_SMA_DEF_PTR(commMgr);
 static CommMgr *me;
+static RKH_EVT_T evtObj, *evt;
+const RKHSmaVtbl rkhSmaVtbl =  /* Instantiate it because rkhsma is mocked */
+{
+    rkh_sma_activate,
+    rkh_sma_dispatch,
+    rkh_sma_post_fifo,
+    rkh_sma_post_lifo
+};
 
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
@@ -40,158 +55,37 @@ void
 setUp(void)
 {
     me = RKH_DOWNCAST(CommMgr, commMgr);
-//    Mock_DigIn_Init();
+    evt = &evtObj;
 }
 
 void
 tearDown(void)
 {
-//    Mock_DigIn_Verify();
-//    Mock_DigIn_Destroy();
 }
 
 void
 test_Initialize(void)
 {
-    RKH_EVT_T *evt;
-
     rkh_trc_ao_Ignore();
     rkh_trc_obj_Ignore();
     rkh_trc_state_Ignore();
+    rkh_trc_state_Ignore();
+    rkh_trc_state_Ignore();
+    rkh_trc_state_Ignore();
+    rkh_trc_state_Ignore();
+    rkh_trc_state_Ignore();
+    rkh_trc_state_Ignore();
+    rkh_trc_state_Ignore();
     rkh_trc_sig_Ignore();
+    rkh_trc_sig_Ignore();
+    rkh_trc_sig_Ignore();
+    rkh_trc_sig_Ignore();
+    rkh_trc_sig_Ignore();
+    rkh_trc_sig_Ignore();
+    rkh_trc_sig_Ignore();
+    rkh_trc_obj_Ignore();
 
-    ConnectionTopic_subscribe_Expect(me);
-    DeviceStatus_subscribe_Expect(me);
-    TEST_ASSERT_EQUAL(0, me->status.pending);
-
-    TEST_ASSERT_EQUAL(0, me->digIn.clk);
-    TEST_ASSERT_EQUAL(0, me->digIn.clkX3);
-    TEST_ASSERT_EQUAL(0, me->digIn.clkX6);
-    TEST_ASSERT_EQUAL(0, me->digIn.failure);
+    CommMgr_ToIdleExt0(me, evt);
 }
 
-#if 0
-void
-test_StartSynchro(void)
-{
-    rkh_tmr_init__Expect(&me->evSyncObj.tmr, (RKH_EVT_T *)&me->evSyncObj);
-    rkh_tmr_start_Expect(&me->evSyncObj.tmr, RKH_UPCAST(RKH_SMA_T, me), 
-                         SIGMON_SYNC_TIME, SIGMON_SYNC_TIME);
-    SigMon_enSMActive(me);
-}
-
-void
-test_StopSynchro(void)
-{
-    rkh_tmr_stop_ExpectAndReturn(&me->evSyncObj.tmr, true);
-    SigMon_exSMActive(me);
-}
-
-void
-test_Synchro(void)
-{
-    DigIn status;
-    rInt inSeqExpect;
-
-    inSeqExpect = 0;
-    me->digIn.clk = status.clk = 1;
-    me->digIn.clkX3 = status.clkX3 = 0;
-    me->digIn.clkX6 = status.clkX6 = 1;
-    me->digIn.failure = status.failure = 0;
-    inSeqExpect = ((status.clk << 2) | (status.clkX3 << 1) | status.clkX6);
-    DigIn_get_ExpectAndReturn(status);
-    rkh_sma_post_lifo_Expect(RKH_UPCAST(RKH_SMA_T, me), &me->evInObj, me);
-
-    SigMon_SMActiveToSMActiveLoc2(me, RKH_UPCAST(RKH_EVT_T, &me->evSyncObj));
-
-    TEST_ASSERT_EQUAL(status.clk, me->digIn.clk);
-    TEST_ASSERT_EQUAL(status.clkX3, me->digIn.clkX3);
-    TEST_ASSERT_EQUAL(status.clkX6, me->digIn.clkX6);
-    TEST_ASSERT_EQUAL(status.failure, me->digIn.failure);
-    TEST_ASSERT_EQUAL(mapDigIn[inSeqExpect], me->evInObj.e);
-}
-
-void
-test_StoreDigInput(void)
-{
-    storeDigInPrepareAndExecute(1, 0);
-    storeDigInPrepareAndExecute(0, 0);
-    storeDigInPrepareAndExecute(6, 1);
-    storeDigInPrepareAndExecute(2, 0);
-    storeDigInPrepareAndExecute(4, 1);
-    storeDigInPrepareAndExecute(0, 0);
-    storeDigInPrepareAndExecute(4, 1);
-    storeDigInPrepareAndExecute(0, 0);
-    storeDigInPrepareAndExecute(4, 1);
-    storeDigInPrepareAndExecute(0, 0);
-    storeDigInPrepareAndExecute(5, 1);
-    storeDigInPrepareAndExecute(1, 0);
-    storeDigInPrepareAndExecute(0, 0);
-    storeDigInPrepareAndExecute(6, 1);
-}
-
-void
-test_StartAdqCycle(void)
-{
-    Relay_getCurrent_ExpectAndReturn(0xdead);
-    Relay_getVoltage_ExpectAndReturn(0xbeaf);
-
-    SigMon_enSeq1(me);
-    TEST_ASSERT_EQUAL(0xdead, me->currVal);
-    TEST_ASSERT_EQUAL(0xbeaf, me->voltVal);
-}
-
-void
-test_AdquireSeq1(void)
-{
-    Relay_getCurrent_ExpectAndReturn(1);
-    Relay_getVoltage_ExpectAndReturn(1);
-    Relay_getCurrent_ExpectAndReturn(2);
-    Relay_getVoltage_ExpectAndReturn(2);
-    Relay_getCurrent_ExpectAndReturn(3);
-    Relay_getVoltage_ExpectAndReturn(3);
-
-    SigMon_enSeq1(me);
-    SigMon_Seq1ToSeq1Loc6(me, RKH_UPCAST(RKH_EVT_T, &me->evSyncObj));
-    SigMon_Seq1ToSeq2Ext7(me, RKH_UPCAST(RKH_EVT_T, &me->evSyncObj));
-
-    TEST_ASSERT_EQUAL(3, me->currVal);
-    TEST_ASSERT_EQUAL(3, me->voltVal);
-}
-
-void
-test_AdquireSeq2(void)
-{
-    Relay_getCurrent_ExpectAndReturn(1);
-    Relay_getVoltage_ExpectAndReturn(1);
-    Relay_getCurrent_ExpectAndReturn(2);
-    Relay_getVoltage_ExpectAndReturn(2);
-
-    SigMon_Seq2ToSeq2Loc5(me, RKH_UPCAST(RKH_EVT_T, &me->evSyncObj));
-    SigMon_Seq2ToSeq2Loc5(me, RKH_UPCAST(RKH_EVT_T, &me->evSyncObj));
-
-    TEST_ASSERT_EQUAL(2, me->currVal);
-    TEST_ASSERT_EQUAL(2, me->voltVal);
-}
-
-void
-test_StopAdqCycle(void)
-{
-    StoreTest_saveRelayStatus_Expect(me->currVal, me->voltVal);
-    SigMon_Seq2ToSeq3Ext8(me, RKH_UPCAST(RKH_EVT_T, &me->evSyncObj));
-}
-
-void
-test_Failure(void)
-{
-    DigIn status;
-
-    StoreTest_saveDigInStatus_Expect(status);
-    StoreTest_saveDigInStatus_IgnoreArg_digIn();
-
-    SigMon_SMActiveToSigMon_FinalExt3(me, 
-                                      RKH_UPCAST(RKH_EVT_T, &me->evSyncObj));
-}
-
-#endif
 /* ------------------------------ End of file ------------------------------ */
