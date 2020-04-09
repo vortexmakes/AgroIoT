632eb3cb23532fc56b51259fc49d7d13820c4916
db <dariosb@gmail.com>
2020-04-09 13:51:29 -0300

    Working on PowerMgr

--- src/PowerMgr/src/PowerMgrAccess.c
+++ src/PowerMgr/src/PowerMgrAccess.c
@@ -1,5 +1,5 @@
 /**
- *  \file       GsmMgrAccess.c
+ *  \file       PowerMgrAccess.c
  *  \brief      Active object's action required interface implementations.
  */
 
@@ -13,14 +13,9 @@
 
 /* --------------------------------- Notes --------------------------------- */
 /* ----------------------------- Include files ----------------------------- */
-#include <string.h>
-
 #include "rkh.h"
-#include "GsmMgr.h"
-#include "GsMMgrAccess.h"
-#include "bsp.h"
-#include "ModCmd.h"
-#include "modpwr.h"
+#include "PowerMgr.h"
+#include "PowerMgrAccess.h"
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
@@ -30,42 +25,4 @@
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
 /* ---------------------------- Global functions --------------------------- */
-void
-powerOn(void)
-{
-    modPwr_on();
-    ModCmd_init();
-}
-
-void
-powerOff(void)
-{
-    modPwr_off();
-    bsp_SIMChange();
-}
-
-void
-modemFound(void)
-{
-    bsp_GSMModemFound();
-}
-
-void
-registered(void)
-{
-    bsp_regStatus(RegisteredSt);
-}
-
-void
-unregistered(void)
-{
-    bsp_regStatus(UnregisteredSt);
-}
-
-void
-stopSMS(void)
-{
-
-}
-
 /* ------------------------------ End of file ------------------------------ */
