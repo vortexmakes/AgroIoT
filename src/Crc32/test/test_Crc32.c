0679e6df1035a6b42a54e8452af1bb5cc57bd104
Leandro Francucci <lf@vortexmakes.com>
2020-04-11 12:28:29 -0300

    Managed overflow detection in Crc32

--- src/Crc32/test/test_Crc32.c
+++ src/Crc32/test/test_Crc32.c
@@ -17,10 +17,21 @@
 #include <string.h>
 #include "unity.h"
 #include "Crc32.h"
+#include "Mock_GStatus.h"
+#include "Mock_rfile.h"
+#include "Mock_Config.h"
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
 /* ---------------------------- Local data types --------------------------- */
+typedef union SecBlock SecBlock;
+union SecBlock
+{
+    FFILE_T ffileData;
+    Config configData;
+    GStatusType gStatusTypeData;
+};
+
 /* ---------------------------- Global variables --------------------------- */
 /* ---------------------------- Local variables ---------------------------- */
 /* ----------------------- Local function prototypes ----------------------- */
@@ -42,6 +53,10 @@ test_CalculateCRC32(void)
     uint8_t message[] = "123456789";
     uint32_t result, expected = 0xcbf43926;
 
+    printf("sizeof FFILE_T: %ld\n", sizeof(FFILE_T));
+    printf("sizeof Config: %ld\n", sizeof(Config));
+    printf("sizeof GStatusType: %ld\n", sizeof(GStatusType));
+    printf("sizeof SecBlock: %ld\n", sizeof(SecBlock));
     Crc32_init();
     result = Crc32_calc(message, strlen(message), 0xffffffff);
 
