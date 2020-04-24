8eeeea126e0afdb84c5a3173644fdfcd028025f0
Leandro Francucci <lf@vortexmakes.com>
2020-04-24 09:37:34 -0300

    Added test stub for Trace module

---
+++ src/ffile/test/support/Trace.c
@@ -0,0 +1,47 @@
+/**
+ *  \file       Trace.c
+ *  \brief      Implementation of application tracing module.
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci lf@vortexmakes.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+#include "Trace.h"
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
+void
+Trace_init(void)
+{
+}
+
+void 
+Trace_send(TraceId id, TraceArg arg0, TraceArg arg1)
+{
+}
+
+void 
+Trace_put(TraceId id, TraceArg arg0, TraceArg arg1)
+{
+}
+
+void 
+Trace_generate(GStatusType *status, TraceId id, TraceArg arg0, TraceArg arg1)
+{
+}
+
+/* ------------------------------ End of file ------------------------------ */
