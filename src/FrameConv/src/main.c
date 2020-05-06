001220d484fb73f1812778ffcdd87af0b16f0bb4
Leandro Francucci <lf@vortexmakes.com>
2020-05-06 16:18:32 -0300

    Working on GStatus layout

--- src/FrameConv/src/main.c
+++ src/FrameConv/src/main.c
@@ -16,9 +16,70 @@
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
+typedef enum ByteOrder ByteOrder;
+enum ByteOrder
+{
+    LittleEndian, BigEndian
+};
+
 /* ---------------------------- Local data types --------------------------- */
+typedef struct FieldLayout FieldLayout;
+struct FieldLayout
+{
+    int offset;
+    int size;
+};
+
+typedef struct GeoLayout GeoLayout;
+struct GeoLayout
+{
+    FieldLayout utc;
+    FieldLayout status;
+    FieldLayout latitude;
+    FieldLayout latInd;
+    FieldLayout longitude;
+    FieldLayout longInd;
+    FieldLayout speed;
+    FieldLayout course;
+    FieldLayout date;
+};
+
+typedef struct GStatusTypeLayout GStatusTypeLayout;
+struct GStatusTypeLayout
+{
+    GeoLayout position;
+    /*CBOX_STRLayout devData;
+    IOStatusLayout ioStatus;
+    BatChrStatusLayout batChrStatus;*/
+};
+
+typedef struct GStatusLayout GStatusLayout;
+struct GStatusLayout
+{
+    GStatusTypeLayout data;
+    FieldLayout checksum;
+};
+
 /* ---------------------------- Global variables --------------------------- */
 /* ---------------------------- Local variables ---------------------------- */
+static GStatusLayout layout =
+{
+    {
+        {
+            {0, 0},
+            {7, 0},
+            {9, 0},
+            {20, 0},
+            {22, 0},
+            {34, 0},
+            {36, 0},
+            {44, 0},
+            {48, 0}
+        },
+    },
+    {}
+};
+
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
 /* ---------------------------- Global functions --------------------------- */
